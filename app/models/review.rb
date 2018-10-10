7560c7f13966a8baf828be5a88fe5fda6971cc51
normanrs <normanrs@live.com>
2018-10-10 14:08:02 -0600

    relationships are hard

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   validates_presence_of :title, :explanation, :score
   belongs_to :user
+  belongs_to :book
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -10,6 +10,7 @@ describe Review, type: :model do
 
   describe 'relationships' do
     it { should belong_to(:user)}
+    it { should belong_to(:book)}
   end
 end
 
