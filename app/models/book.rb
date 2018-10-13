da9fde09c665a54733fffaae62a2813398876210
normanrs <normanrs@live.com>
2018-10-13 17:26:58 -0600

    updated seed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,30 +1,37 @@
 class Book < ApplicationRecord
-  validates_presence_of :title, :pages, :year
-  has_many :author_books
-  has_many :authors, through: :author_books
-  has_many :reviews
+ validates_presence_of :title, :pages, :year
+ has_many :author_books
+ has_many :authors, through: :author_books
+ has_many :reviews
 
-  def reviews_count
-    reviews.count
-  end
+ def reviews_count
+   if reviews.count == nil
+     then result = 0.0
+   else result = reviews.count
+   end
+   result
+ end
 
-  def average_score
-    reviews.average(:score)
-  end
+ def average_score
+   if reviews.average(:score) == nil
+     then result = 0.0
+   else result = reviews.average(:score).to_i.round(1)
+   end
+   result
+ end
 
-  def sort_reviews(dir)
-    reviews.order("score #{dir}").limit(3)
-  end
+ def sort_reviews(dir)
+   reviews.order("score #{dir}").limit(3)
+ end
 
-  def self.sortby(criteria, dir)
-    if criteria == :page_num
-      order(pages: dir)
-    elsif criteria ==
-    :avg_rating
-      select('books.*, avg(reviews.score) as average_score').joins(:reviews).group(:id).order("average_score #{dir}")
-    elsif criteria == :review_num
-      select('books.*, reviews.count as count').joins(:reviews).group(:id).order("count #{dir}")
-    end
-  end
+ def self.sortby(criteria, dir)
+   if criteria == :page_num
+     order(pages: dir)
+   elsif criteria == :avg_rating
+     select('books.*, avg(reviews.score) as average_score').left_outer_joins(:reviews).group(:id).order("average_score #{dir}")
+   elsif criteria == :review_num
+     select('books.*, reviews.count as count').left_outer_joins(:reviews).group(:id).order("count #{dir}")
+   end
+ end
 
 end
