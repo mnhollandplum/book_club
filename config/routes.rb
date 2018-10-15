4aac8ef49262154687f1bcea5e4a73ea90c41c26
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-10-15 14:59:45 -0600

    possible trash

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :books
-  resources :user, only:[:index]
-  resources :authors
+  resources :books, only:[:index, :show, :new, :create]
+  resources :user, only:[:show]
+  resources :authors, only:[:show]
 
 end
