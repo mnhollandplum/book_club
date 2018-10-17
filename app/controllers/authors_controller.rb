c7a7ab6ca2b3d4a69093142f6794f8e3ee0ce425
normanrs <normanrs@live.com>
2018-10-16 18:55:09 -0600

    completed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,15 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
     @books = Book.where(id: @author.id)
   end
+
+  def destroy
+    gone_author = Author.find(params[:id])
+    gone_author.books.each do |book|
+      book.destroy if book.authors.length == 1
+    end
+    AuthorBook.where(author_id: params['id']).destroy_all
+    Author.destroy(params['id'])
+    redirect_to books_path
+  end
+
 end
